5858fc88b7ac1802b2d0e8d7a6cf09af74fe26cc
Retno Wati <retnowati3099@gmail.com>
2023-05-29 12:52:56 +0700

    tambah sweet alert

--- Frontend/src/crud/UpdateArticle/index.jsx
+++ Frontend/src/crud/UpdateArticle/index.jsx
@@ -1,89 +1,90 @@
 // import React from 'react'
-import { useEffect, useState } from 'react'
-import { Link, useNavigate, useParams } from 'react-router-dom'
-import swal from 'sweetalert'
-import instance from '../../api/api_instace'
-import { Button, Gap } from '../../components'
+import { useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import swal from "sweetalert";
+import instance from "../../api/api_instace";
+import { Button, Gap } from "../../components";
 // const api = `http://13.239.136.211/blog-api/v1/article/update`;
 
 const UpdateArticle = () => {
   const [update, setUpdate] = useState({
-    title: '',
-    description: '',
-  })
+    title: "",
+    description: "",
+  });
 
-  const { id_article } = useParams()
-  const navigate = useNavigate()
+  const { id_article } = useParams();
+  const navigate = useNavigate();
 
   // get article
   useEffect(() => {
-    document.title = 'Update article'
+    document.title = "Update article";
     instance
       .get(`article/${id_article}`)
       .then((res) => {
-        console.log(res)
-        setUpdate(res.data.data)
+        console.log(res);
+        setUpdate(res.data.data);
       })
       .catch((err) => {
-        console.error(err)
-      })
-  }, [id_article])
+        console.error(err);
+      });
+  }, [id_article]);
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    console.log('diklik')
+    e.preventDefault();
+    console.log("diklik");
 
     // update article
     instance
       .put(`/article/update/${id_article}`, update)
       .then((res) => {
-        console.log(res.data)
+        console.log(res.data);
         // tampilkan notifikasi data berhasil diupdate menggunakan sweet alert
+
         // alert(res.data.message)
         swal({
-          title: 'Success!',
-          text: 'Data Article Berhasil Diupdate!',
-          icon: 'success',
-          button: 'close!',
-        })
-        navigate('/')
+          title: "Success!",
+          text: "Data Artikel Berhasil Diupdate!",
+          icon: "success",
+          button: "close!",
+        });
+        navigate("/");
       })
       .catch((err) => {
-        console.error(err)
-      })
-  }
+        console.error(err);
+      });
+  };
 
   return (
     <>
-      <div className='container'>
+      <div className="container">
         <Gap height={170} />
-        <h1 className='text-center mb-2 title'> Update Article</h1>
+        <h1 className="text-center mb-2 title"> Update Article</h1>
         <br />
-        <div className='row justify-content-center bg-head'>
-          <div className='col-md-6'>
-            <div className='card'>
-              <div className='card-header text-center text-bg'>
+        <div className="row justify-content-center bg-head">
+          <div className="col-md-6">
+            <div className="card">
+              <div className="card-header text-center text-bg">
                 Form Update Article
               </div>
-              <div className='card-body'>
+              <div className="card-body">
                 <form onSubmit={handleSubmit}>
-                  <div className='form-group'>
-                    <label htmlFor=''>Title</label>
+                  <div className="form-group">
+                    <label htmlFor="">Title</label>
                     <input
-                      type='text'
-                      className='form-control'
-                      placeholder='Enter Title...'
+                      type="text"
+                      className="form-control"
+                      placeholder="Enter Title..."
                       value={update.title}
                       onChange={(e) =>
                         setUpdate({ ...update, title: e.target.value })
                       }
                     />
                   </div>
-                  <div className='form-group'>
-                    <label htmlFor=''>Description</label>
+                  <div className="form-group">
+                    <label htmlFor="">Description</label>
                     <textarea
-                      className='form-control'
-                      placeholder='Enter Description...'
+                      className="form-control"
+                      placeholder="Enter Description..."
                       value={update.description}
                       onChange={(e) =>
                         setUpdate({ ...update, description: e.target.value })
@@ -91,19 +92,19 @@ const UpdateArticle = () => {
                     ></textarea>
                   </div>
                   <Gap height={10} />
-                  <div className='btn-group'>
-                    <Link to='/'>
+                  <div className="btn-group">
+                    <Link to="/">
                       <Button
-                        icon='arrow-left'
-                        label='Back '
-                        variant='outline-secondary'
+                        icon="arrow-left"
+                        label="Back "
+                        variant="outline-secondary"
                       />
                     </Link>
                     <Gap width={20} />
                     <Button
-                      label='Update Article'
-                      variant='outline-primary'
-                      className='color'
+                      label="Update Article"
+                      variant="outline-primary"
+                      className="color"
                     />
                   </div>
                 </form>
@@ -113,7 +114,7 @@ const UpdateArticle = () => {
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default UpdateArticle
+export default UpdateArticle;
